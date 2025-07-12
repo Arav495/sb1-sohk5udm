cfedc46a3411741b01e948c93772fcb90387e786
Arav495 <vinayagroindustry@gmail.com>
2025-07-12 21:57:09 +0530

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ujbytwqmhwcducxtkmix.supabase.co'; // ⬅️ replace this
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqYnl0d3FtaHdjZHVjeHRrbWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMzYyNzIsImV4cCI6MjA2NzkxMjI3Mn0.yN6eJo_IIsUq7HUxjwKp2WzL6mi3EvdQrwaWTXsSF4w'; // ⬅️ your anon public key
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
